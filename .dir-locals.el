9ba701b35c4255d77cf44077d0246712c8ca5f49
Matthew Schlegel <mkschleg@gmail.com>
2022-02-09 16:21:42 -0700

    added dir locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((org-mode . ((bibtex-completion-bibliography . ("./rnns.bib"
+                                                 "./rl.bib"
+                                                 "./me.bib"
+                                                 "./candidacy.bib"
+                                                 "./predreps.bib")))))
